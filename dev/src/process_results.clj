fda2d99f4f6dcd5d81a8eee1ccc0a2535318e641
Chris Nuernberger <chris@techascent.com>
2022-09-10 17:30:59 -0600

    Rounding values to make README easier to digest.

--- dev/src/process_results.clj
+++ dev/src/process_results.clj
@@ -14,6 +14,22 @@
        (remove #(= % ".keepme"))
        (map #(str "results/" %))))
 
+(def numeric-columns (drop 2 perftest/column-order))
+
+
+(defn round-columns
+  [ds cnames ndigits]
+  (let [ndigits (double (Math/pow 10.0 ndigits))]
+    (reduce (fn [ds cname]
+              (ds/column-map ds cname (fn [val]
+                                        (when val
+                                          (-> (* (double val) ndigits)
+                                              (Math/round)
+                                              (/ ndigits))))
+                             [cname]))
+            ds
+            cnames)))
+
 
 (defn result-file->dataset
   [fname]
@@ -23,6 +39,7 @@
                :dataset
                (ds/->dataset)
                (ds/select-columns perftest/column-order)
+               (round-columns numeric-columns 3)
                ;;Always print entire dataset
                (ds/head 10000))]
     (vary-meta ds merge (-> (dissoc data :dataset)
