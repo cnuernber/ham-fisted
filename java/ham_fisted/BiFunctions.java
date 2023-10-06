58bd7cedc79d1612bd1fa23b52bd0f690fcbccae
Chris Nuernberger <chris@techascent.com>
2023-10-06 10:44:17 -0600

    reformatting

--- java/ham_fisted/BiFunctions.java
+++ java/ham_fisted/BiFunctions.java
@@ -5,7 +5,6 @@ import java.util.function.BiFunction;
 
 
 public class BiFunctions {
-  public static final BiFunction incBiFn = (k, v)-> v == null ? Long.valueOf(1) :
-    Long.valueOf(((long)v) + 1);
+  public static final BiFunction incBiFn = (k, v)-> v == null ? Long.valueOf(1) : Long.valueOf(((long)v) + 1);
   public static final BiFunction rhsWins = (v1,v2)->v2;
 }
