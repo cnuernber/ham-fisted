4cebabd6d15c5d72c8855335f97666a109f62bd0
Chris Nuernberger <chris@techascent.com>
2023-10-06 10:41:33 -0600

    Adding useful downstream fns

---
+++ java/ham_fisted/BiFunctions.java
@@ -0,0 +1,11 @@
+package ham_fisted;
+
+
+import java.util.function.BiFunction;
+
+
+public class BiFunctions {
+  public static final BiFunction incBiFn = (k, v)-> v == null ? Long.valueOf(1) :
+    Long.valueOf(((long)v) + 1);
+  public static final BiFunction rhsWins = (v1,v2)->v2;
+}
