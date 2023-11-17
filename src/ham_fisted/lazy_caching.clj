efc94968daf1637bbc06b2097110e450bdd8a936
Chris Nuernberger <chris@techascent.com>
2023-11-16 17:06:25 -0700

    Adding chunking to higher arity maps does improve their performance.

--- src/ham_fisted/lazy_caching.clj
+++ src/ham_fisted/lazy_caching.clj
@@ -2,39 +2,80 @@
   (:require [ham-fisted.lazy-noncaching :as lznc])
   (:import [java.util RandomAccess List Iterator]
            [ham_fisted Transformables$CachingIterable Transformables$CachingList
-            ArrayLists ArrayHelpers ArrayLists$ObjectArrayList]
-           [clojure.lang ISeq ArraySeq IFn])
+            ArrayLists ArrayHelpers ArrayLists$ObjectArrayList LazyChunkedSeq]
+           [clojure.lang ISeq ArraySeq IFn ChunkedCons ArrayChunk])
   (:refer-clojure :exclude [map filter concat repeatedly]))
+
+
+(defmacro lazy-chunked-seq
+  [chunked-cons-code]
+  `(LazyChunkedSeq. (fn [] ~chunked-cons-code)))
+
 
 (defn- seq-map-recur
   ([f ^ISeq c1 ^ISeq c2]
-   (when (and c1 c2)
-     (cons (f (.first c1) (.first c2))
-           (lazy-seq (seq-map-recur f (.next c1) (.next c2))))))
+   (let [chunk-data (ArrayLists/objectArray 32)]
+     (loop [c1 c1
+            c2 c2
+            idx 0]
+       (if (and c1 c2 (< idx 32))
+         (do
+           (ArrayHelpers/aset chunk-data idx (f (.first c1) (.first c2)))
+           (recur (.next c1) (.next c2) (unchecked-inc idx)))
+         (ChunkedCons. (ArrayChunk. chunk-data 0 idx)
+                       (when (and c1 c2)
+                         (lazy-chunked-seq (seq-map-recur f c1 c2))))))))
   ([f ^ISeq c1 ^ISeq c2 ^ISeq c3]
-   (when (and c1 c2 c3)
-     (cons (f (.first c1) (.first c2) (.first c3))
-           (lazy-seq (seq-map-recur f (.next c1) (.next c2) (.next c3))))))
+   (let [chunk-data (ArrayLists/objectArray 32)]
+     (loop [c1 c1
+            c2 c2
+            c3 c3
+            idx 0]
+       (if (and c1 c2 c3 (< idx 32))
+         (do
+           (ArrayHelpers/aset chunk-data idx (f (.first c1) (.first c2) (.first c3)))
+           (recur (.next c1) (.next c2) (.next c3 )(unchecked-inc idx)))
+         (ChunkedCons. (ArrayChunk. chunk-data 0 idx)
+                       (when (and c1 c2 c3)
+                         (lazy-chunked-seq (seq-map-recur f c1 c2 c3))))))))
   ([f ^ISeq c1 ^ISeq c2 ^ISeq c3 ^ISeq c4]
-   (when (and c1 c2 c3 c4)
-     (cons (f (.first c1) (.first c2) (.first c3) (.first c4))
-           (lazy-seq (seq-map-recur f (.next c1) (.next c2) (.next c3) (.next c4))))))
+   (let [chunk-data (ArrayLists/objectArray 32)]
+     (loop [c1 c1
+            c2 c2
+            c3 c3
+            c4 c4
+            idx 0]
+       (if (and c1 c2 c3 c4 (< idx 32))
+         (do
+           (ArrayHelpers/aset chunk-data idx (f (.first c1) (.first c2) (.first c3) (.first c4)))
+           (recur (.next c1) (.next c2) (.next c3) (.next c4) (unchecked-inc idx)))
+         (ChunkedCons. (ArrayChunk. chunk-data 0 idx)
+                       (when (and c1 c2 c3 c4)
+                         (lazy-chunked-seq (seq-map-recur f c1 c2 c3 c4))))))))
   ([f ^List cs]
-   (let [ncs (.size cs)
-         args (ArrayLists/objectArray ncs)         
-         next? (loop [idx 0 next? true]
-                 (if (< idx ncs)
-                   (let [^ISeq c (.get cs (unchecked-int idx))
-                         _ (ArrayHelpers/aset args (unchecked-int idx) (.first c))
-                         c (.next c)]               
-                     (.set cs (unchecked-int idx) c)
-                     (recur (unchecked-inc idx) (and next? c)))
-                   next?))]
-     (cons (.applyTo ^IFn f (ArraySeq/create args))
-           (when next? 
-             (lazy-seq (seq-map-recur f cs)))))))
+   (let [chunk-data (ArrayLists/objectArray 32)
+         ncs (.size cs)
+         args (ArrayLists/objectArray ncs)
+         [cidx next?]
+         (loop [cidx 0
+                next? true]
+           (if (and next? (< cidx 32))
+             (let [next? (loop [idx 0 next? true]
+                           (if (< idx ncs)
+                             (let [^ISeq c (.get cs (unchecked-int idx))
+                                   _ (ArrayHelpers/aset args (unchecked-int idx) (.first c))
+                                   c (.next c)]
+                               (.set cs (unchecked-int idx) c)
+                               (recur (unchecked-inc idx) (and next? c)))
+                             next?))]
+               (ArrayHelpers/aset chunk-data cidx (.applyTo ^IFn f (ArraySeq/create args)))
+               (recur (unchecked-inc cidx) next?))
+             [cidx next?]))]
+     (ChunkedCons. (ArrayChunk. chunk-data 0 cidx)
+                   (when next?
+                     (lazy-chunked-seq (seq-map-recur f cs)))))))
 
-(defn- seq-map
+(defn ^:no-doc seq-map
   ([f arg] (clojure.core/map f arg))
   ([f c1 c2]
    (let [^ISeq c1 (seq c1)
@@ -64,12 +105,12 @@
          c3 (seq c3)
          c4 (seq c4)]
      (if (and c1 c2 c3 c4)
-       (do 
+       (do
          (.add cs c1)
          (.add cs c2)
          (.add cs c3)
          (.add cs c4)
-         (let [all-valid? 
+         (let [all-valid?
                (reduce (fn [acc v]
                          (if-let [s (seq v)]
                            (do (.add cs s)
@@ -85,7 +126,7 @@
 
 (defn map
   ([f arg] (clojure.core/map f arg))
-  ([f c1 c2] (->> (lznc/map f c1 c2) (seq)))
+  ([f c1 c2] (seq-map f c1 c2))
   ([f c1 c2 c3] (seq-map f c1 c2 c3))
   ([f c1 c2 c3 c4] (seq-map f c1 c2 c3 c4))
   ([f c1 c2 c3 c4 & args] (seq-map f c1 c2 c3 c4 args)))
