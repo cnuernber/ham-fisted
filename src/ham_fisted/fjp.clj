37cbbede492ba3612c3ea69e3e255cee503bc9b3
Chris Nuernberger <chris@techascent.com>
2026-04-23 15:41:41 -0600

    Updated document for fjp and spliterator namespaces

--- src/ham_fisted/fjp.clj
+++ src/ham_fisted/fjp.clj
@@ -1,4 +1,26 @@
 (ns ham-fisted.fjp
+  "Support for java.util.concurrent.ForkJoinPool-specific operations such as managed block and task fork/join.
+  Additionally supports concept of 'exception-safe' via wrapping executing code and unwrapper result post
+  execution in order avoid wrapping exceptions and breaking calling code that may be expecting specific
+  exception types.
+
+  Some of the api's fall back to regular executor service code when called.
+
+  Example:
+
+```clojure
+(defn split-parallel-reduce
+  \"Perform a parallel reduction of a spliterator using the provided ExecutorService\"
+  [executor-service split ideal-split init-fn rfn merge-fn]
+  (let [n-elems (proto/estimate-count split)
+        pool (or executor-service (ForkJoinPool/commonPool))]
+    (if (or (<= n-elems (long ideal-split)) (= n-elems Long/MAX_VALUE))
+      (split-reduce rfn (init-fn) split)
+      (if-let [[lhs rhs] (proto/split split)]
+        (let [lt (fjp/safe-fork-task pool (split-parallel-reduce pool lhs ideal-split init-fn rfn merge-fn))
+              rt (fjp/safe-fork-task pool (split-parallel-reduce pool rhs ideal-split init-fn rfn merge-fn))]
+          (merge-fn (fjp/managed-block-unwrap lt) (fjp/managed-block-unwrap rt)))))))
+```"
   (:require [ham-fisted.language :refer [cond not]]
             [ham-fisted.protocols :as proto]
             [ham-fisted.defprotocol :refer [extend-type extend-protocol]])
@@ -11,8 +33,12 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
-(defn common-pool ^ForkJoinPool [] (ForkJoinPool/commonPool))
-(defn parallelism ^long [] (ForkJoinPool/getCommonPoolParallelism))
+(defn common-pool "Returns  (ForkJoinPool/commonPool)"
+  ^ForkJoinPool [] (ForkJoinPool/commonPool))
+(defn common-pool-parallelism "Integer parallelism assigned to the common pool "
+  ^long [] (ForkJoinPool/getCommonPoolParallelism))
+(defn in-fork-join-pool? "Returns true if this task is executing in a fork join pool thread"
+  [] (ForkJoinTask/inForkJoinPool))
 
 (extend-protocol proto/ManagedBlocker
   ForkJoinTask
@@ -38,7 +64,7 @@
 
 (defn managed-block
   "Block on a delay or future using the fjp system's managed blocking facility.  Safe to call all the time
-  whether the current system is blocking or not."
+  whether the current system is in a forkjoinpool task or not."
   ([dly]
    (if (instance? ForkJoinTask dly)
      (.join ^ForkJoinTask dly)
@@ -53,19 +79,30 @@
                     clojure.lang.IDeref
                     (deref [this] (get-value))))))
 
-(defn task
-  "Create a task from a clojure IFn or something that implements IDeref"
+(defn task "Create a task from a clojure IFn or something that implements IDeref"
   ^FJTask [f] (FJTask. f))
 
-(defn fork
-  "submit a task for execution - call from within a running task"
-  [^ForkJoinTask t] (.fork t))
+(defn fork-task
+  "Begin a separate execution for f.  If already in a fork join pool fork the task else
+  submit f to passed in pool."
+  [pool f] (if (in-fork-join-pool?)
+             (let [t (task f)] (.fork t) t)
+             (.submit ^ExecutorService pool ^Callable f)))
 
-(defn fork-task [pool f] (if (instance? ForkJoinPool pool)
-                           (let [t (task f)] (.fork t) t)
-                           (.submit ^ExecutorService pool ^Callable f)))
+(defmacro exception-safe
+  "Wrap code in an exception-safe wrapper - returns a map with either 
+  `:ham-fisted.fjp/result` or `:ham-fisted.fjp.error`."
+  [& code]  
+  `(let [~(with-meta 'ffn {:tag 'Callable})
+         (^:once fn* []
+          (try {:ham-fisted.fjp/result (do ~@code)}
+               (catch Throwable e# {:ham-fisted.fjp/error e#})))]
+     ~'ffn))
 
-(defmacro safe-fork-task [pool & code]
+(defmacro safe-fork-task
+  "Called from within an executing task, fork a executing some code and wrapping it in [[exception-safe]] 
+  then calling [[fork-task]]"
+  [pool & code]
   `(->> (do ~@code)
         (exception-safe)
         (fork-task ~pool)))
@@ -88,17 +125,8 @@
   (-> (.submit (ForkJoinPool/commonPool) ^Callable code)
       (deref)))
 
-(defmacro exception-safe
-  "Wrap code in an exception-safe wrapper - returns a map with either 
-  `:ham-fisted.fjp/result` or `:ham-fisted.fjp.error`."
-  [& code]  
-  `(let [~(with-meta 'ffn {:tag 'Callable})
-         (^:once fn* []
-          (try {:ham-fisted.fjp/result (do ~@code)}
-               (catch Throwable e# {:ham-fisted.fjp/error e#})))]
-     ~'ffn))
-
 (defn unwrap-safe
+  "Unwrap result created via executing code wrapped in [[exception-safe]].  Throws original exception if found."
   [m]
   (let [rv (get m ::result ::failure)]
     (if (identical? rv ::failure)
--- src/ham_fisted/spliterator.clj
+++ src/ham_fisted/spliterator.clj
@@ -1,9 +1,10 @@
 (ns ham-fisted.spliterator
+  "Support for spliterator reduction and parallel reduction."
   (:require [ham-fisted.protocols :as proto]
             [ham-fisted.fjp :as fjp]
             [ham-fisted.defprotocol :refer [extend-type]]
             [ham-fisted.language :as hamf-language])
-  (:import [java.util Spliterator Spliterator$OfDouble Spliterator$OfLong]
+  (:import [java.util Spliterator Spliterator$OfDouble Spliterator$OfLong List RandomAccess]
            [java.util.function Consumer]
            [java.util.concurrent ForkJoinPool]
            [clojure.lang IFn$DDD IFn$LLL]
@@ -26,22 +27,28 @@
   proto/Split
   (split [m] nil))
 
-(defmacro deref-long-consumer [varname accept-code deref-code]
+(defmacro deref-long-consumer
+  "Create a LongConsumer with support for IDeref"
+  [varname accept-code deref-code]
   `(reify Consumers$IDerefLongConsumer
      (acceptLong [_ ~varname] (let [l# ~accept-code]))
      (deref [_] ~deref-code)))
 
-(defmacro deref-double-consumer [varname accept-code deref-code]
+(defmacro deref-double-consumer
+  "Create a DoubleConsumer with support for IDeref"
+  [varname accept-code deref-code]
   `(reify Consumers$IDerefDoubleConsumer
      (acceptDouble [_ ~varname] (let [l# ~accept-code]))
      (deref [_] ~deref-code)))
 
-(defmacro deref-consumer [varname accept-code deref-code]
+(defmacro deref-consumer
+  "Create a Consumer with support for IDeref"
+  [varname accept-code deref-code]
   `(reify Consumers$IDerefConsumer
      (accept [_ ~varname] ~accept-code)
      (deref [_] ~deref-code)))
 
-(defn split-reduce [rfn acc split]
+(defn split-reduce "Reduce over a spliterator.  Special support exists for IFn$LLL and IFn$DDD" [rfn acc split]
   (cond
     (instance? IFn$LLL rfn)
     (let [dd (long-array [(Casts/longCast acc)])
@@ -55,15 +62,15 @@
       @cc)
     :else
     (let [dd (hamf-language/obj-ary acc)
-          cc (deref-consumer ll (aset dd 0 (rfn (aget dd 0) )) (aget dd 0))]
+          cc (deref-consumer ll (aset dd 0 (rfn (aget dd 0) ll)) (aget dd 0))]
       (loop []
         (let [c? (.tryAdvance ^Spliterator split cc)]
-          (when c?
+          (if c?
             (let [vv @cc]
               (if (reduced? vv)
                 @vv
-                (recur))))))
-      @cc)))
+                (recur)))
+            (unreduced @cc)))))))
 
 (clojure.core/extend Spliterator
     clojure.core.protocols/CollReduce
@@ -72,14 +79,27 @@
 (defn- println-ret [v] (println v) v)
 
 (extend-type Spliterator
+  proto/ToSpliterator
+  (->spliterator [s] s)
   proto/EstimateCount
   (estimate-count [s] (.estimateSize s))
   proto/Split
   (split [s] (when-let [ss (.trySplit s)] [s ss])))
 
-(defn split-parallel-reduce [pool split ideal-split init-fn rfn merge-fn]
+(extend-type java.util.RandomAccess
+  proto/Split
+  (split [s] (let [^List s s
+                   ne (.size s)]
+               (when (> ne 2)
+                 (let [n (quot ne 2)]
+                   [(.subList s 0 n)
+                    (.subList s n ne)])))))
+
+(defn split-parallel-reduce
+  "Perform a parallel reduction of a spliterator using the provided ExecutorService"
+  [executor-service split ideal-split init-fn rfn merge-fn]
   (let [n-elems (proto/estimate-count split)
-        pool (or pool (ForkJoinPool/commonPool))]
+        pool (or executor-service (ForkJoinPool/commonPool))]
     (if (or (<= n-elems (long ideal-split)) (= n-elems Long/MAX_VALUE))
       (split-reduce rfn (init-fn) split)
       (if-let [[lhs rhs] (proto/split split)]
