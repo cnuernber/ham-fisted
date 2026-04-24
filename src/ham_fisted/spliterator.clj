dd6ba710690ece3855e51f87252ba0f5b064e8ef
Chris Nuernberger <chris@techascent.com>
2026-04-24 11:52:41 -0600

    final spliterator updates

--- src/ham_fisted/spliterator.clj
+++ src/ham_fisted/spliterator.clj
@@ -4,7 +4,7 @@
             [ham-fisted.fjp :as fjp]
             [ham-fisted.defprotocol :refer [extend-type]]
             [ham-fisted.language :as hamf-language])
-  (:import [java.util Spliterator Spliterator$OfDouble Spliterator$OfLong List RandomAccess]
+  (:import [java.util Spliterator Spliterator$OfDouble Spliterator$OfLong List RandomAccess ArrayList]
            [java.util.function Consumer]
            [java.util.concurrent ForkJoinPool]
            [clojure.lang IFn$DDD IFn$LLL]
@@ -48,33 +48,46 @@
      (accept [_ ~varname] ~accept-code)
      (deref [_] ~deref-code)))
 
-(defn split-reduce "Reduce over a spliterator.  Special support exists for IFn$LLL and IFn$DDD" [rfn acc split]
-  (cond
-    (instance? IFn$LLL rfn)
-    (let [dd (long-array [(Casts/longCast acc)])
-          cc (deref-long-consumer ll (aset dd 0 (.invokePrim ^IFn$LLL rfn (aget dd 0) ll)) (aget dd 0))]
-      (.forEachRemaining ^Spliterator split cc)
-      @cc)
-    (instance? IFn$DDD rfn)
-    (let [dd (double-array [(Casts/doubleCast acc)])
-          cc (deref-double-consumer ll (aset dd 0 (.invokePrim ^IFn$DDD rfn (aget dd 0) ll)) (aget dd 0))]
-      (.forEachRemaining ^Spliterator split cc)
-      @cc)
-    :else
-    (let [dd (hamf-language/obj-ary acc)
-          cc (deref-consumer ll (aset dd 0 (rfn (aget dd 0) ll)) (aget dd 0))]
-      (loop []
-        (let [c? (.tryAdvance ^Spliterator split cc)]
-          (if c?
-            (let [vv @cc]
-              (if (reduced? vv)
-                @vv
-                (recur)))
-            (unreduced @cc)))))))
+(defn ->spliterator ^Spliterator [ii] (if (instance? Spliterator ii) ii (proto/->spliterator ii)))
+
+(defn split-reduce "Reduce over a spliterator.  Special support exists for IFn$LLL and IFn$DDD"
+  ([rfn split]
+   (let [acc-ary (object-array 1)
+         cc (deref-consumer ll (aset acc-ary 0 ll) (aget acc-ary 0))
+         split (->spliterator split)]
+     (if (.tryAdvance split cc)
+       (split-reduce rfn (aget acc-ary 0) split)
+       (rfn))))
+  ([rfn acc split]
+   (cond
+     (instance? IFn$LLL rfn)
+     (let [dd (long-array [(Casts/longCast acc)])
+           cc (deref-long-consumer ll (aset dd 0 (.invokePrim ^IFn$LLL rfn (aget dd 0) ll)) (aget dd 0))]
+       (.forEachRemaining (->spliterator split) cc)
+       @cc)
+     (instance? IFn$DDD rfn)
+     (let [dd (double-array [(Casts/doubleCast acc)])
+           cc (deref-double-consumer ll (aset dd 0 (.invokePrim ^IFn$DDD rfn (aget dd 0) ll)) (aget dd 0))]
+       (.forEachRemaining (->spliterator split) cc)
+       @cc)
+     :else
+     (let [dd (hamf-language/obj-ary acc)
+           cc (deref-consumer ll (aset dd 0 (rfn (aget dd 0) ll)) (aget dd 0))
+           split (->spliterator split)]
+       (loop []
+         (let [c? (.tryAdvance split cc)]
+           (if c?
+             (let [vv @cc]
+               (if (reduced? vv)
+                 @vv
+                 (recur)))
+             (unreduced @cc))))))))
 
 (clojure.core/extend Spliterator
-    clojure.core.protocols/CollReduce
-    {:coll-reduce split-reduce})
+  clojure.core.protocols/CollReduce
+  {:coll-reduce (fn reduce-spliterator
+                  ([coll rfn acc] (split-reduce rfn acc coll))
+                  ([coll rfn] (split-reduce rfn coll)))})
 
 (defn- println-ret [v] (println v) v)
 
@@ -98,14 +111,16 @@
 (defn split-parallel-reduce
   "Perform a parallel reduction of a spliterator using the provided ExecutorService"
   [executor-service split ideal-split init-fn rfn merge-fn]
-  (let [n-elems (proto/estimate-count split)
+  (let [split (->spliterator split)
+        n-elems (.estimateSize split)
         pool (or executor-service (ForkJoinPool/commonPool))]
     (if (or (<= n-elems (long ideal-split)) (= n-elems Long/MAX_VALUE))
       (split-reduce rfn (init-fn) split)
-      (if-let [[lhs rhs] (proto/split split)]
-        (let [lt (fjp/safe-fork-task pool (split-parallel-reduce pool lhs ideal-split init-fn rfn merge-fn))
+      (if-let [rhs (.trySplit split)]
+        (let [lt (fjp/safe-fork-task pool (split-parallel-reduce pool split ideal-split init-fn rfn merge-fn))
               rt (fjp/safe-fork-task pool (split-parallel-reduce pool rhs ideal-split init-fn rfn merge-fn))]
-          (merge-fn (fjp/managed-block-unwrap lt) (fjp/managed-block-unwrap rt)))))))
+          (merge-fn (fjp/managed-block-unwrap lt) (fjp/managed-block-unwrap rt)))
+        (split-reduce rfn (init-fn) split)))))
 
 (extend-type Spliterator
   proto/ParallelReduction
@@ -115,3 +130,48 @@
 (extend-type java.util.Collection
   proto/ToSpliterator
   (->spliterator [c] (.spliterator c)))
+
+(comment
+
+  ;;alternative reduction - couldn't get it to perform as well in simple tests
+  (defn split-to-max-size [split ^long max-size op]
+    (let [split (->spliterator split)
+          n-elems (.estimateSize ^Spliterator split)]
+      (if (or (<= n-elems max-size) (== n-elems Long/MAX_VALUE))
+        [(op split)]
+        (let [rv (doto (ArrayList. ) (.add split))]
+          (loop [idx 0]
+            (if (== idx (.size rv))
+              rv
+              (let [ll (.get rv idx)]
+                (if (<= (.estimateSize ^Spliterator ll) max-size)
+                  (do
+                    (.set rv idx (op ll))
+                    (recur (inc idx)))
+                  (if-let [rhs (.trySplit ^Spliterator ll)]
+                    (do (.add rv (inc idx) rhs)
+                        (recur idx))
+                    (do
+                      (.set rv idx (op ll))
+                      (recur (inc idx))))))))))))
+
+  (defn ^:no-doc ms-parallel-reduce
+    "Perform a parallel reduction of a spliterator using the provided ExecutorService"
+    [executor-service split ideal-split init-fn rfn merge-fn]
+    (let [split (->spliterator split)
+          n-elems (.estimateSize split)
+          pool (or executor-service (ForkJoinPool/commonPool))
+          ideal-split (long ideal-split)]
+      (if (or (<= n-elems ideal-split) (= n-elems Long/MAX_VALUE))
+        (split-reduce rfn (init-fn) split)
+        (let [data (split-to-max-size
+                    split ideal-split
+                    #(fjp/safe-fork-task pool (split-parallel-reduce pool % ideal-split init-fn rfn merge-fn)))
+              merge-fn #(reduce merge-fn (lznc/map fjp/managed-block-unwrap %))]
+          (loop [merge-data data]
+            (if (>= (count merge-data) ideal-split)
+              (recur (split-to-max-size
+                      merge-data ideal-split
+                      #(fjp/safe-fork-task pool (merge-fn %))))
+              (merge-fn merge-data)))))))
+  )
